5d1261b27b6c8921a9f92a1c9d517dff533d503b
Erkka Mutanen <erkka.mutanen@sievo.com>
2018-04-13 14:20:31 +0300

    Add InfoTitle component. Shows if all is good / there is issue.

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
 import AppContainer from './AppContainer';
 import LineChart from './Components/LineChart';
+import InfoTitle from './Components/InfoTitle';
 import logo from './logo.svg';
 import './App.css';
 import LoadingSpinner from './Components/LoadingSpinner';
@@ -9,9 +10,19 @@ import 'bootstrap/dist/css/bootstrap.min.css';
 
 class App extends Component {
 	componentDidMount() {
-		service.fetchNodesMock().then((data) => {
-			this.setState({ data });
-		});
+		service
+			.fetchNodesMock()
+			.then((data) => {
+				this.setState({ data });
+			})
+			.then(() => {
+				this.setState({ foundIssue: false });
+				this.state.data.nodes.map((node) => {
+					if (node.statusCode == 'DOWN') {
+						this.setState({ foundIssue: true });
+					}
+				});
+			});
 	}
 
 	render() {
@@ -20,6 +31,12 @@ class App extends Component {
 				<header className="App-header">
 					<h1 className="App-title">Drill All The Humans</h1>
 				</header>
+				<div className="Info-title">
+					<InfoTitle
+						dataReady={this.state != null && this.state.data != null}
+						nodesStatus={this.state && this.state.foundIssue ? this.state.foundIssue : null}
+					/>
+				</div>
 				<div className="App-intro">
 					{this.state != null && this.state.data != null ? (
 						<AppContainer nodes={this.state.data.nodes} />
--- src/utils/service.js
+++ src/utils/service.js
@@ -1,40 +1,40 @@
 const service = {
-  fetchNodes: () => {
-    const url = "http://anttim.northeurope.cloudapp.azure.com:3003/api/result";
-    fetch(url)
-      .then(response => {
-        return response.json();
-      })
-      .then(r => console.log(r));
-  },
-  fetchNodesMock: () => {
-    return new Promise(resolve => {
-      const data = {
-        nodes: [
-          {
-            statusCode: "UP",
-            nodeName: "QV-Node-v1",
-          },
-          {
-            statusCode: "DOWN",
-            nodeName: "QV-Node-v2",
-          },
-          {
-            statusCode: "DOWN",
-            nodeName: "QV-Node-v3",
-          },
-          {
-            statusCode: "UP",
-            nodeName: "QV-Node-v4",
-          },
-        ],
-      };
+	fetchNodes: () => {
+		const url = 'http://anttim.northeurope.cloudapp.azure.com:3003/api/result';
+		fetch(url)
+			.then((response) => {
+				return response.json();
+			})
+			.then((r) => console.log(r));
+	},
+	fetchNodesMock: () => {
+		return new Promise((resolve) => {
+			const data = {
+				nodes: [
+					{
+						statusCode: 'UP',
+						nodeName: 'QV-Node-v1'
+					},
+					{
+						statusCode: 'DOWN',
+						nodeName: 'QV-Node-v2'
+					},
+					{
+						statusCode: 'UP',
+						nodeName: 'QV-Node-v3'
+					},
+					{
+						statusCode: 'UP',
+						nodeName: 'QV-Node-v4'
+					}
+				]
+			};
 
-      setTimeout(function() {
-        resolve(data);
-      }, 500);
-    });
-  },
+			setTimeout(function() {
+				resolve(data);
+			}, 500);
+		});
+	}
 };
 
 export default service;
