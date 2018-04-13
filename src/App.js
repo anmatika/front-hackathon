6692575a1f75069326d9c6fdc309c1fb05e9ba5a
Antti Matikainen <antti.matikainen@sievo.com>
2018-04-13 13:39:33 +0300

    support routes

--- src/App.js
+++ src/App.js
@@ -1,4 +1,6 @@
 import React, { Component } from "react";
+
+import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
 import NodesPage from "./NodesPage";
 import LineChart from "./Components/LineChart";
 import logo from "./logo.svg";
@@ -15,7 +17,11 @@ class App extends Component {
           <h1 className="App-title">Drill All The Humans</h1>
         </header>
         <div className="App-intro">
-          <NodesPage />
+          <Router basename={"/"}>
+            <Switch>
+              <Route exact path="/" render={() => <NodesPage />} />
+            </Switch>
+          </Router>
         </div>
       </div>
     );
