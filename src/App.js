22e81115f8ee823ab30286929603cfdc4d07a2bd
Antti Matikainen <antti.matikainen@sievo.com>
2018-04-13 13:34:02 +0300

    structure

--- src/App.js
+++ src/App.js
@@ -1,35 +1,25 @@
-import React, { Component } from 'react';
-import AppContainer from './AppContainer';
-import LineChart from './Components/LineChart';
-import logo from './logo.svg';
-import './App.css';
-import LoadingSpinner from './Components/LoadingSpinner';
-import service from './utils/service';
-import 'bootstrap/dist/css/bootstrap.min.css';
+import React, { Component } from "react";
+import NodesPage from "./NodesPage";
+import LineChart from "./Components/LineChart";
+import logo from "./logo.svg";
+import "./App.css";
+import LoadingSpinner from "./Components/LoadingSpinner";
+import service from "./utils/service";
+import "bootstrap/dist/css/bootstrap.min.css";
 
 class App extends Component {
-	componentDidMount() {
-		service.fetchNodesMock().then((data) => {
-			this.setState({ data });
-		});
-	}
-
-	render() {
-		return (
-			<div className="App">
-				<header className="App-header">
-					<h1 className="App-title">Drill All The Humans</h1>
-				</header>
-				<div className="App-intro">
-					{this.state != null && this.state.data != null ? (
-						<AppContainer nodes={this.state.data.nodes} />
-					) : (
-						<LoadingSpinner />
-					)}
-				</div>
-			</div>
-		);
-	}
+  render() {
+    return (
+      <div className="App">
+        <header className="App-header">
+          <h1 className="App-title">Drill All The Humans</h1>
+        </header>
+        <div className="App-intro">
+          <NodesPage />
+        </div>
+      </div>
+    );
+  }
 }
 
 export default App;
