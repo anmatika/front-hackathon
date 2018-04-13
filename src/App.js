daf1d6a003a0546f065f28d77e1f76f20f273ea3
Erkka Mutanen <erkka.mutanen@sievo.com>
2018-04-13 12:56:11 +0300

    Add uuidv4 keys to NodeList component. Change App-intro to div.

--- src/App.js
+++ src/App.js
@@ -1,52 +1,52 @@
-import React, { Component } from "react";
-import AppContainer from "./AppContainer";
-import LineChart from "./Components/LineChart";
-import logo from "./logo.svg";
-import "./App.css";
-import "bootstrap/dist/css/bootstrap.min.css";
+import React, { Component } from 'react';
+import AppContainer from './AppContainer';
+import LineChart from './Components/LineChart';
+import logo from './logo.svg';
+import './App.css';
+import 'bootstrap/dist/css/bootstrap.min.css';
 
 class App extends Component {
-  componentDidMount() {
-    const data = {
-      nodes: [
-        {
-          statusCode: "UP",
-          statusText: "QV-Node-v1",
-        },
-        {
-          statusCode: "DOWN",
-          statusText: "QV-Node-v2",
-        },
-        {
-          statusCode: "DOWN",
-          statusText: "QV-Node-v3",
-        },
-        {
-          statusCode: "UP",
-          statusText: "QV-Node-v4",
-        },
-      ],
-    };
+	componentDidMount() {
+		const data = {
+			nodes: [
+				{
+					statusCode: 'UP',
+					statusText: 'QV-Node-v1'
+				},
+				{
+					statusCode: 'DOWN',
+					statusText: 'QV-Node-v2'
+				},
+				{
+					statusCode: 'DOWN',
+					statusText: 'QV-Node-v3'
+				},
+				{
+					statusCode: 'UP',
+					statusText: 'QV-Node-v4'
+				}
+			]
+		};
 
-    this.setState({ data });
-  }
+		this.setState({ data });
+	}
 
-  render() {
-    return (
-      <div className="App">
-        <header className="App-header">
-          <h1 className="App-title">Drill All The Humans</h1>
-        </header>
-        <p className="App-intro">
-          {this.state != null && this.state.data != null ? (
-            <AppContainer nodes={this.state.data.nodes} />
-          ) : (
-            "Loading..."
-          )}
-        </p>
-      </div>
-    );
-  }
+	render() {
+		return (
+			<div className="App">
+				<header className="App-header">
+					<h1 className="App-title">Drill All The Humans</h1>
+				</header>
+				<div className="App-intro">
+					{this.state != null && this.state.data != null ? (
+						<AppContainer nodes={this.state.data.nodes} />
+					) : (
+						'Loading...'
+					)}
+				</div>
+			</div>
+		);
+	}
 }
 
 export default App;
--- src/Components/NodeList.js
+++ src/Components/NodeList.js
@@ -1,34 +1,35 @@
-import React from "react";
-import { Line } from "react-chartjs-2";
-import { Row, Col } from "reactstrap";
-import { Card, CardImg, CardText, CardBody, CardTitle, CardSubtitle, Button } from "reactstrap";
-import Node from "./Node";
+import React from 'react';
+import { Line } from 'react-chartjs-2';
+import { Row, Col } from 'reactstrap';
+import { Card, CardImg, CardText, CardBody, CardTitle, CardSubtitle, Button } from 'reactstrap';
+import Node from './Node';
+const uuidv4 = require('uuid/v4');
 
-const NodeList = props => {
-  return (
-    <Row>
-      <Col xs="6">
-        <Card>
-          <CardTitle>Nodes Up</CardTitle>
-          <CardBody>
-            {props.nodes
-              .filter(c => c.statusCode === "UP")
-              .map(c => <Node statusText={c.statusText} statusCode={c.statusCode} />)}
-          </CardBody>
-        </Card>
-      </Col>
-      <Col xs="6">
-        <Card>
-          <CardTitle>Nodes Down</CardTitle>
-          <CardBody>
-            {props.nodes
-              .filter(c => c.statusCode === "DOWN")
-              .map(c => <Node statusText={c.statusText} statusCode={c.statusCode} />)}
-          </CardBody>
-        </Card>
-      </Col>
-    </Row>
-  );
+const NodeList = (props) => {
+	return (
+		<Row>
+			<Col xs="6">
+				<Card>
+					<CardTitle>Nodes Up</CardTitle>
+					<CardBody>
+						{props.nodes
+							.filter((c) => c.statusCode === 'UP')
+							.map((c) => <Node key={uuidv4()} statusText={c.statusText} statusCode={c.statusCode} />)}
+					</CardBody>
+				</Card>
+			</Col>
+			<Col xs="6">
+				<Card>
+					<CardTitle>Nodes Down</CardTitle>
+					<CardBody>
+						{props.nodes
+							.filter((c) => c.statusCode === 'DOWN')
+							.map((c) => <Node key={uuidv4()} statusText={c.statusText} statusCode={c.statusCode} />)}
+					</CardBody>
+				</Card>
+			</Col>
+		</Row>
+	);
 };
 
 export default NodeList;
