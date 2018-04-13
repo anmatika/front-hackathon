929f50661207b724ac33e89c4da499aa1e04a871
Antti Matikainen <antti.matikainen@sievo.com>
2018-04-13 12:54:11 +0300

    add loading spinner

--- src/App.js
+++ src/App.js
@@ -3,32 +3,15 @@ import AppContainer from "./AppContainer";
 import LineChart from "./Components/LineChart";
 import logo from "./logo.svg";
 import "./App.css";
+import LoadingSpinner from "./Components/LoadingSpinner";
+import service from "./utils/service";
 import "bootstrap/dist/css/bootstrap.min.css";
 
 class App extends Component {
   componentDidMount() {
-    const data = {
-      nodes: [
-        {
-          statusCode: "UP",
-          nodeName: "QV-Node-v1",
-        },
-        {
-          statusCode: "DOWN",
-          nodeName: "QV-Node-v2",
-        },
-        {
-          statusCode: "DOWN",
-          nodeName: "QV-Node-v3",
-        },
-        {
-          statusCode: "UP",
-          nodeName: "QV-Node-v4",
-        },
-      ],
-    };
-
-    this.setState({ data });
+    service.fetchNodesMock().then(data => {
+      this.setState({ data });
+    });
   }
 
   render() {
@@ -41,7 +24,7 @@ class App extends Component {
           {this.state != null && this.state.data != null ? (
             <AppContainer nodes={this.state.data.nodes} />
           ) : (
-            "Loading..."
+            <LoadingSpinner />
           )}
         </p>
       </div>
